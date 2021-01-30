1a7e3551be2777a21ab5eea84dd7d570be243d5f
Ahmad Tariq <ahmadbintariq4u@gmail.com>
2021-01-30 14:09:36 +0500

    Completed_Lecture_4

---
+++ lec4/lec_4.js
@@ -0,0 +1,57 @@
+// DataTypes in Javascript
+
+// Primitive DataTypes in Javascript
+
+var name='Ahmad';
+console.log('My String is: '+name);
+console.log('My DataType is : '+(typeof name))
+
+{
+let name='Ahmad';
+console.log('My String is: '+name);
+console.log('My DataType is : '+(typeof name))
+}
+
+
+let age=18;
+console.log('My DataType is : '+(typeof age))
+
+let is=true;
+console.log('My DataType is : '+(typeof true))
+
+let isnull=null;
+console.log('My DataType is : '+(typeof isnull))
+
+let undef=undefined;
+console.log('My DataType is : '+(typeof undef))
+
+
+
+// Reference dataTypes
+// arrays
+let myarr=[1,2,3,4,false,'i am here']
+console.log(myarr)
+console.log('My DataType is : '+(typeof myarr))
+
+// object Literals
+let marks={
+
+hamad:90,
+ahmad:100,
+'ahmad tariq':1000
+
+
+}
+
+console.log(marks)
+console.log('My DataType is : '+(typeof marks))
+
+
+function nameA(){
+
+}
+console.log('My DataType is : '+(typeof nameA))
+
+let date=new Date();
+console.log('My DataType is : '+(typeof date))
+

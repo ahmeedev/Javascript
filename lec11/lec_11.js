905cfaeb7675e35aeb110506cdeb85640c0f1659
Ahmad Tariq <ahmadbintariq4u@gmail.com>
2021-02-04 14:34:41 +0500

    completed_Lecture_11
    Basic Window Object practice .

---
+++ lec11/lec_11.js
@@ -0,0 +1,24 @@
+console.log('welcome to lecture-11')
+
+let hold=window;
+console.log(hold)
+
+// let a=prompt('Type a name of the User');
+// console.log(a)
+
+// let b=confirm('This is a deme confirmation')
+// console.log(b)
+
+// console.log(window.innerHeight)
+// console.log(window.innerWidth)
+
+console.log(scrollX)
+console.log(scrollY)
+
+// console.log(location.reload())
+
+
+//location.href="https://github.com/ahmadbintariq4u/Javascript"
+
+console.log(hold)
+

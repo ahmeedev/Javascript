5be0205ca892f1fb9a417f354cfb8399b583b57d
ahmadbintariq4u <ahmadbintariq4u@gmail.com>
2021-02-06 16:19:26 +0500

    Lecture 12+13 Completed
    Understanding DOM and other concept.
    
    Also, i install node js for File handling in javascript.

---
+++ lec12/lec_12+13.js
@@ -0,0 +1,43 @@
+console.log('hello i\'m in lecture_12')
+
+let a=document.all;
+
+// Array.from(a).forEach(function(element){
+
+// console.log(element)
+
+// });
+
+// a=document.images[0].baseURI;
+// document.location.href=a;
+
+
+// a=document.images;
+
+let text='google';
+let b;
+
+console.log(document.links.length)
+for(let i=0; i<document.links.length; i++){
+    
+    a=document.links[i].href;
+    console.log(a);
+
+    if(document.links[i].href.includes(text))
+    b=document.links[i].href;
+}
+
+console.log('this link contains google keyword')
+console.log(b);
+
+
+
+
+
+//var fs=require('fs');
+
+
+
+
+
+

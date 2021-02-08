642d4174c9f44316aaded4465dcbfdf6763ac270
ahmadbintariq4u <ahmadbintariq4u@gmail.com>
2021-02-08 08:27:40 +0500

    Completed_Lec_15
    Basic Traversing && Manipulation with DOM

---
+++ lec14/lec_15.js
@@ -0,0 +1,16 @@
+console.log('Welcome to Lecture_15');
+
+let hold=document.querySelector('.container')
+
+console.log(hold.children[0].children)
+console.log(hold.nextElementSibling)
+console.log(hold.nextSibling)
+console.log(hold.lastSibling)
+console.log(hold.parentElement)
+console.log(hold.childElementCount)
+console.log(hold.lastChild)
+console.log(hold.hasChildNodes())
+console.log(hold.hasAttributes())
+
+console.log(hold.nodeType)
+console.log(hold.nodeName)
